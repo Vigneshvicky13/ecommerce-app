280e87141b27b6169f9ffdeea93e3b09e3811296
Vignesh A <vigneshandamuthu@gmail.com>
2026-09-04 21:06:08 +0530

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/contact";
+import Product from "./pages/product";
+import Cart from "./pages/cart";
+import Login from "./pages/login";
+import Placeorder from "./pages/Placeorder";
+import Orders from "./pages/orders";
+import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Verify from "./pages/Verify";
+
+const App = () => {
+  return (
+     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
+      <Navbar/>
+      <SearchBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path ='/contact' element={<Contact/>} />
+        <Route path='/product/:productId' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<Placeorder />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/verify' element={<Verify/>} />
+
+
+      </Routes>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Orders.jsx
@@ -0,0 +1,133 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+const Orders = () => {
+
+  const { backendUrl, token, currency } = useContext(ShopContext)
+
+  const [orderData, setOrderData] = useState([])
+
+  const loadOrderData = async () => {
+
+    try {
+
+      if (!token) {
+        return null
+      }
+
+      const response = await axios.post(
+        backendUrl + '/api/order/userorders',
+        {},
+        { headers: { token } }
+      )
+
+      if (response.data.success) {
+
+        let allOrderItems = []
+
+        response.data.orders.forEach((order) => {
+
+          order.items.forEach((item) => {
+
+            item.status = order.status
+            item.payment = order.payment
+            item.paymentMethod = order.paymentMethod
+            item.date = order.date
+
+            allOrderItems.push(item)
+
+          })
+
+        })
+
+        setOrderData(allOrderItems.reverse())
+
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
+  }
+
+  useEffect(() => {
+    loadOrderData()
+  }, [token])
+
+  return (
+    <div className='border-t pt-16'>
+
+      <div className='text-2xl'>
+        <Title text1={'MY'} text2={'ORDERS'} />
+      </div>
+
+      <div>
+
+        {
+          orderData.map((item, index) => (
+
+            <div
+              key={index}
+              className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_2fr_1fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'
+            >
+
+              <div className='flex items-start gap-6 text-sm'>
+
+                <img
+                  className='w-16 sm:w-20'
+                  src={item.image[0]}
+                  alt=""
+                />
+
+                <div>
+
+                  <p className='sm:text-base font-medium'>
+                    {item.name}
+                  </p>
+
+                  <div className='flex items-center gap-3 mt-2 text-base text-gray-500'>
+                    <p>{currency}{item.price}</p>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Size: {item.size}</p>
+                  </div>
+
+                  <p className='mt-1'>
+                    Date: <span className='text-gray-400'>{new Date(item.date).toDateString()}</span>
+                  </p>
+
+                  <p className='mt-1'>
+                    Payment: <span className='text-gray-400'>{item.paymentMethod}</span>
+                  </p>
+
+                </div>
+
+              </div>
+
+              <div className='flex items-center gap-2'>
+                <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
+                <p className='text-sm md:text-base'>{item.status}</p>
+              </div>
+
+              <button
+                onClick={loadOrderData}
+                className='border px-4 py-2 text-sm font-medium rounded-sm'
+              >
+                Track Order
+              </button>
+
+            </div>
+
+          ))
+        }
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Orders
