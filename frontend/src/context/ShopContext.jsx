f1aeb119f8b1dbbd90d3e03db3ee4bf006121d23
Vigneshvicky13 <vigneshandamuthu@gmail.com>
2026-09-05 08:03:34 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL.replace(/\/+$/, "");
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     const [token, setToken] = useState('');
     const [products, setProducts] = useState([]);
